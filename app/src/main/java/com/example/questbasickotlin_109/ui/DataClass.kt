b603025f126e8800ea3c4646a60076168034d7a1
PutriHabibahDevina <putrihdm13@gmail.com>
2024-10-13 09:48:31 +0700

    Menambahkan komen

--- app/src/main/java/com/example/questbasickotlin_109/ui/DataClass.kt
+++ app/src/main/java/com/example/questbasickotlin_109/ui/DataClass.kt
@@ -15,14 +15,16 @@ fun main() {
     println(data)
 
     // Fungsi Equals
+    // Membuat objek kedua dengan id yang sama tetapi email yang berbeda
     val data2 = DataClass(1, "makan@gmail.com")
+    // Membandingkan dua objek menggunakan operator '==', yang pada Data Class membandingkan isi (value) dari objek
     println(data == data2) // false
 
     // Fungsi Copy
-    val data3 = data.copy()
+    val data3 = data.copy() // Membuat salinan dari objek data tanpa perubahan
     println(data3)
 
     // Fungsi Copy dengan perubahan
-    val data4 = data.copy(email = "sahar@gmail.com")
+    val data4 = data.copy(email = "sahar@gmail.com") // Membuat salinan dari objek data dengan mengubah properti email menjadi "sahar@gmail.com"
     println(data4)
 }
